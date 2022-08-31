a46bdc9f8be3f1a41277590db9e4ea7205a4c53c
Bohdan <Upworksbv@gmail.com>
2022-08-30 22:38:29 -0500

    initial commit

--- pages/api/freemask.js
+++
@@ -1,4 +0,0 @@
-export default async function send(req, res) {
-    const { pid } = req.query
-    res.redirect('/product-add-ondiscount?pid='+pid)
-}
--- pages/api/get-product.js
+++
@@ -1,8 +0,0 @@
-import { getVariantProduct } from '@/lib/shopify'
-
-export default async function getDiscountProd(req, res) {
-
-  const prod = await getVariantProduct(req.body.id)
-
-  res.status(200).json({ product: prod })
-}
